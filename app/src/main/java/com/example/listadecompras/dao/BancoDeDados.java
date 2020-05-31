9d5e0659c38ec115e2179b3d43e081903d4f3191
Fernanda <fernandaraeli@gmail.com>
2020-05-31 17:45:35 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/listadecompras/dao/BancoDeDados.java
@@ -0,0 +1,24 @@
+package com.example.listadecompras.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BancoDeDados extends SQLiteOpenHelper {
+
+    public BancoDeDados(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
