5544b3071e8e180f8c2ab32809e847ab6d7dacf8
deisemoraesf <deise.moraesfarias@gmail.com>
2020-05-31 23:44:13 -0300

    CRUD ok

--- app/src/main/java/com/example/listadecompras/MainActivity.java
+++ app/src/main/java/com/example/listadecompras/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         listaProdutos();
 
-
         minhaLista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
             @Override
